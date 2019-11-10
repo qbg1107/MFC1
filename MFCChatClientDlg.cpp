a6403567b1bd2c35bc7606e597fe1deff1a7bed8
qbg <82835844@qq.com>
2019-11-11 02:23:15 +0800

    wd

--- MFCChatClientDlg.cpp
+++ MFCChatClientDlg.cpp
@@ -170,7 +170,7 @@ void CMFCChatClientDlg::OnBnClickedDisconnectBtn()
 	// TODO: 在此添加控件通知处理程序代码
 	TRACE("##OnBnClickedDisconnectBtn");
 	TRACE("##O22nBnClickedDisconnectBtn");
-	TRACE("##33O22nBnClickedDisconnectBtn");
+	TRACE("##33O22nB55nClickedDisconnectBtn");
 
 }
 
